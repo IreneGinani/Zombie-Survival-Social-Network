39bccf0164148d39f168869beafe614d8d18b582
ireneginani <ireneginani@gmail.com>
2018-07-06 16:27:50 -0300

    simplification on serialization for survivor model

--- restApi/serializers.py
+++ restApi/serializers.py
@@ -3,13 +3,6 @@ from .models import Survivor
 
 
 class SurvivorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    
-    def create(self, validated_data):
-        return  Survivor.objects.create(**validated_data)
-    
-    def update(self, validated_data):
-        instance.title = validated_data.get('longitude', instance.longitude)
-        instance.save()
-        return instance
+    class Meta:
+        model = Survivor
+        fields = ('id','name', 'age', 'gender', 'longitude', 'latitude', 'is_infected')
