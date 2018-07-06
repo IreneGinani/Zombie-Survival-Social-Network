6b6911cf5d931914d0656fc8f69246cd31e21ca1
ireneginani <ireneginani@gmail.com>
2018-07-06 13:16:46 -0300

    survivor model created

---
+++ restApi/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Survivor(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    
+    class Meta:
+        verbose_name = u'Survivor'
+        verbose_name_plural = u'Survivors'
+        
+    def __str__(self):
+        return self.name
+  
+  
