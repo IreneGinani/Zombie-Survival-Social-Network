92d3c79ed74be96ba6792fdbb6d3146e4c7a248b
ireneginani <ireneginani@gmail.com>
2018-07-06 17:25:39 -0300

    method create a survivor location update

--- restApi/views.py
+++ restApi/views.py
@@ -11,9 +11,9 @@ from restApi.models import Survivor
 from restApi.serializers import SurvivorSerializer
 
 @csrf_exempt
-def survivor_list(request):
+def survivor_create(request):
     """
-    List all code survivors, or create a new snippet.
+    List all code survivors, or create a new survivor.
     """
     if request.method == 'GET':
         survivor = Survivor.objects.all()
@@ -26,4 +26,22 @@ def survivor_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def survivor_update(request, pk):
+    """
+    Retrieve, update or delete a code survivor.
+    """
+    try:
+        survivor = Survivor.objects.get(pk=pk)
+    except Survivor.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = Survivor_LocationSerializer(survivor, data=data)
+        if serializer.is_valid():
+            survivor.save()
+            return JsonResponse(survivor.data)
         return JsonResponse(serializer.errors, status=400)
