adb00a212b596da1ea81b5a0c106009226296063
ireneginani <ireneginani@gmail.com>
2018-07-06 14:32:10 -0300

    initial test for item model created

--- restApi/tests/test_models.py
+++ restApi/tests/test_models.py
@@ -11,3 +11,14 @@ class TestSurvivor(TestCase):
   def test_record_creation(self):
     self.assertTrue(isinstance(self.survivor, Survivor))
     self.assertEquals(self.survivor.__str__(), self.survivor.name)
+
+class TestItem(TestCase):
+  
+  def setUp(self):
+        self.item = mommy.make(Item, name='Food')
+      
+  def test_genre_creation(self):
+        self.assertTrue(isinstance(self.item, item))
+        self.assertEquals(self.item.__str__(), self.genre.item)
+        
+        
