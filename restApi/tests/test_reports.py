59230be4f4f81024a5e4bd9b8b56b82081eff968
ireneginani <ireneginani@gmail.com>
2018-07-09 18:43:27 -0300

    test for lost points from infected survivors metho created

--- restApi/tests/test_reports.py
+++ restApi/tests/test_reports.py
@@ -118,4 +118,76 @@ class TestReports(APITestCase):
         self.assertEquals(json.loads(request.content)['Percentage of items per survivor']['food'],0.5)
         self.assertEquals(json.loads(request.content)['Percentage of items per survivor']['ammunition'],0.5)
         self.assertEquals(json.loads(request.content)['Percentage of items per survivor']['medication'],0.5)
+        self.assertEquals(request.status_code, 200)
+
+    def test_pointslost_total(self):
+        survivor1 = self.client.post('http://localhost:8000/api/v1/survivor/', {
+                                                    "name": "Mario Fernandes",
+                                                    "age": 30,
+                                                    "gender": "M",
+                                                    "latitude": -16.346867430274,
+                                                    "longitude": -48.948227763174,
+                                                    "is_infected": True,
+                                                    "count_reports":0,
+                                                    "inventory": {
+  		                                                            "inventory_items": [
+	  	                                                            {"id": 1 },
+		                                                            {"id": 3 }
+		                                                            ]
+                                                                }
+                                                    }, format='json')
+        survivor2 = self.client.post('http://localhost:8000/api/v1/survivor/', {
+                                                    "name": "Mario Fernandes",
+                                                    "age": 30,
+                                                    "gender": "M",
+                                                    "latitude": -16.346867430274,
+                                                    "longitude": -48.948227763174,
+                                                    "is_infected": True,
+                                                    "count_reports":0,
+                                                    "inventory": {
+  		                                                            "inventory_items": [
+	  	                                                            {"id": 2 },
+		                                                            {"id": 4 }
+		                                                            ]
+                                                                }
+                                                    }, format='json')
+        request = self.client.get('http://localhost:8000/api/v1/survivor/points_lost/')
+
+        self.assertEquals(json.loads(request.content)['Total lost points'],10)
+        self.assertEquals(request.status_code, 200)
+
+    def test_pointslost_perperson(self):
+        survivor1 = self.client.post('http://localhost:8000/api/v1/survivor/', {
+                                                    "name": "Mario Fernandes",
+                                                    "age": 30,
+                                                    "gender": "M",
+                                                    "latitude": -16.346867430274,
+                                                    "longitude": -48.948227763174,
+                                                    "is_infected": True,
+                                                    "count_reports":0,
+                                                    "inventory": {
+  		                                                            "inventory_items": [
+	  	                                                            {"id": 1 },
+		                                                            {"id": 3 }
+		                                                            ]
+                                                                }
+                                                    }, format='json')
+        survivor2 = self.client.post('http://localhost:8000/api/v1/survivor/', {
+                                                    "name": "Mario Fernandes",
+                                                    "age": 30,
+                                                    "gender": "M",
+                                                    "latitude": -16.346867430274,
+                                                    "longitude": -48.948227763174,
+                                                    "is_infected": True,
+                                                    "count_reports":0,
+                                                    "inventory": {
+  		                                                            "inventory_items": [
+	  	                                                            {"id": 2 },
+		                                                            {"id": 4 }
+		                                                            ]
+                                                                }
+                                                    }, format='json')
+        request = self.client.get('http://localhost:8000/api/v1/survivor/points_lost/1/')
+
+        self.assertEquals(json.loads(request.content)['Total lost points from this survivor'],5)
         self.assertEquals(request.status_code, 200)    
